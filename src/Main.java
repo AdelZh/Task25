0488963090e4b25cd0a71a97882ca87499408412
Dell <adelyajyldyzbekova@gmail.com>
2023-08-07 14:00:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        int x=scan.nextInt();
+
+        while(x>0) {
+            int num = scan.nextInt();
+            x -= num;
+            System.out.println(x);
+
+
+
+
+            
+        }
+            
+    }}
